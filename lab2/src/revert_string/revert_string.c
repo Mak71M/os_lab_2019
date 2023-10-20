6060da7488e7a36c642ae908558700e0729df51c
Mak71M <94022564+Mak71M@users.noreply.github.com>
2023-10-20 12:24:42 +0000

    odin

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
-#include "revert_string.h"
-
 void RevertString(char *str)
 {
-	// your code here
+	char *start = str;
+	char *end = str + strlen(str) - 1;
+
+	while (start < end)
+	{
+		char temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
+	}
 }
 
