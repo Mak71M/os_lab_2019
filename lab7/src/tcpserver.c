f359e3c6cc8e8ce0ffea95ff6a09baa7b220a50f
Mak71M <94022564+Mak71M@users.noreply.github.com>
2024-02-02 19:22:27 +0000

    lab7

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -1,7 +1,9 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <getopt.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -11,12 +13,51 @@
 #define BUFSIZE 100
 #define SADDR struct sockaddr
 
-int main() {
+int main(int argc, char *argv[]) {
+  int port = -1;
+  int buff_size = -1; 
   const size_t kSize = sizeof(struct sockaddr_in);
+  while (true){
+    int current_optind = optind ? optind : 1;
+    static struct option options[] = {{"port", required_argument, 0, 0},
+                                      {"buff_size", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
+    if (c == -1)
+      break;
+    
+    switch(c){
+      case 0:
+        switch(option_index){
+          case 0:
+            port = atoi(optarg);
+            break;
+          case 1:
+            buff_size = atoi(optarg);
+            break;
+          default:
+            printf("Index %d is out of options\n", option_index);
+        }
+        break;
+      case '?':
+        printf("Unknown argument\n");
+        break;
+      default:
+        fprintf(stderr, "getopt returned character code 0%o?\n", c);
+    }
+  }
 
+   if (port == -1 || buff_size == -1) {
+    printf("Using: --port 20001 --buff_size 100\n");
+    port = SERV_PORT;
+    buff_size = BUFSIZE;
+    printf("The default params are port = %d and buff_size %d\n", SERV_PORT, BUFSIZE);
+  }
+  
   int lfd, cfd;
   int nread;
-  char buf[BUFSIZE];
+  char buf[buff_size];
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
 
@@ -28,7 +69,7 @@ int main() {
   memset(&servaddr, 0, kSize);
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
+  servaddr.sin_port = htons(port);
 
   if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
     perror("bind");
@@ -49,7 +90,7 @@ int main() {
     }
     printf("connection established\n");
 
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
+    while ((nread = read(cfd, buf, buff_size)) > 0) {
       write(1, &buf, nread);
     }
 
